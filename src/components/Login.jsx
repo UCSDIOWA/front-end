fd12fc78572d229b2c12357583a179355386a9b8
Njl002 <nathaniellee51@gmail.com>
2018-10-23 17:35:50 -0700

    outline of file structure

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from React;
+import {Button, Icon} from 'semantic-ui-react';
+import {login} from '../server/api';
+
+export class LoginButton extends Component {
+render() {
+  < div className='login-button'>
+    <>
+  </div>
+  
+}
