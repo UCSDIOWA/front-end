2b57095d62dcfd99c83ebd3c65347a8b2861e9b9
GarrettBoulais <gboulais@ucsd.edu>
2018-10-29 23:21:03 -0700

    Add files via upload

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+} from 'semantic-ui-react'
+import HolderImage from '../holder-image.jpg'
+
+const NavBar = () => (
+  <div>
+    <Menu fixed='top' inverted>
+      <Container textAlign='right'>
+      
+      <Menu.Item as='a' header>
+          <Image size='mini' src={HolderImage} style={{ marginRight: '1.5em' }} /> 
+        </Menu.Item>
+        <Dropdown item simple text='Profile'>
+        
+          <Dropdown.Menu>
+            <Dropdown.Item>Settings</Dropdown.Item>
+            <Dropdown.Item>Logout</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Container>
+    </Menu>
+
+    
+  </div>
+)
+
+export default NavBar
+  
