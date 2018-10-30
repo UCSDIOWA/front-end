12ee2ac94b01cb9e8c281333fefb13ad06a272ca
GarrettBoulais <gboulais@ucsd.edu>
2018-10-30 16:41:41 -0700

    removed unused files

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import {
   Container,
-  Divider,
   Dropdown,
-  Grid,
-  Header,
   Image,
-  List,
   Menu,
-  Segment,
 } from 'semantic-ui-react'
 import HolderImage from '../holder-image.jpg'
 
