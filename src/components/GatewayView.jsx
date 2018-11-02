ac4a44cc768dd2345928321d7d25c2371ab9516a
Njl002 <nathaniellee51@gmail.com>
2018-11-01 17:30:24 -0700

    added basic router functionality via the url and created empty components for others to work on

---
+++ src/components/GatewayView.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Icon} from 'semantic-ui-react';
+
+export default class GatewayView extends Component {
+  render() {
+
+    return (
+      <div>
+        <Icon name='attention' size='massive' />
+        <h1>
+          Gateway View Work in Progress
+        </h1>
+      </div>
+    );
+  }
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom';
 import LoginView from './LoginView';
-import NavBar from './NavBar';
-
+import GatewayView from './GatewayView';
+import SignUpView from './SignUpView';
+import ProfileView from './ProfileView';
+import {navConsts} from '../constants';
 
 export default class Main extends Component {
   render() {
+    const {GATEWAY, SIGNUP, PROFILE} = navConsts;
+
     return (
         <main className="App-container">
-          <NavBar />
-          <LoginView />
+          <Switch>
+            <Route exact path='/' component={LoginView} />
+            <Route exact path={'/' + GATEWAY} component={GatewayView} />
+            <Route exact path={'/' + SIGNUP} component={SignUpView} />
+            <Route exact path={'/' + PROFILE} component={ProfileView} />
+          </Switch>
         </main>
     );
   }
