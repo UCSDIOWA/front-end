48eeedfc5b7a3d5cbdd1e195d8625141592bbc72
GarrettBoulais <gboulais@ucsd.edu>
2018-11-04 11:59:58 -0800

    Trying to get Nate's updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import LoginView from './LoginView';
 import NavBar from './NavBar';
-
+import CreateProject from './CreateProject';
+import { Container } from 'semantic-ui-react';
 
 export default class Main extends Component {
   render() {
     return (
         <main className="App-container">
-          <NavBar />
-          <LoginView />
+        <NavBar />
+        <CreateProject />
         </main>
     );
   }
