29ce84e3ac9422e30d2e246124bbf7c872b79b6c
GarrettBoulais <gboulais@ucsd.edu>
2018-11-04 14:37:53 -0800

    working on routing, and CreateProjectView

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,10 @@
 import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import LoginView from './LoginView';
-<<<<<<< HEAD
-import NavBar from './NavBar';
-import CreateProject from './CreateProject';
-import { Container } from 'semantic-ui-react';
-=======
 import GatewayView from './GatewayView';
 import SignUpView from './SignUpView';
 import ProfileView from './ProfileView';
 import {navConsts} from '../constants';
->>>>>>> origin
 
 export default class Main extends Component {
   render() {
@@ -18,17 +12,12 @@ export default class Main extends Component {
 
     return (
         <main className="App-container">
-<<<<<<< HEAD
-        <NavBar />
-        <CreateProject />
-=======
           <Switch>
             <Route exact path='/' component={LoginView} />
             <Route exact path={'/' + GATEWAY} component={GatewayView} />
             <Route exact path={'/' + SIGNUP} component={SignUpView} />
             <Route exact path={'/' + PROFILE} component={ProfileView} />
           </Switch>
->>>>>>> origin
         </main>
     );
   }
