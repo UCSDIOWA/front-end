5efb33c8dfb16a5aa96c2d37d9db9f3c23cc0b3f
GarrettBoulais <gboulais@ucsd.edu>
2018-11-27 19:26:49 -0800

    dont know

--- src/components/CreateProjectView.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import CreateProjectForm from "./CreateProjectForm";
-
-export default class CreateProjectView extends Component {
-  render() {
-    return <CreateProjectForm />;
-  }
-}
